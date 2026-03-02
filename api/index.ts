9003732d73874cb422ddd4a8d1466b3e6a817fc0
Joenel <joenelsevellejo831@gmail.com>
2026-03-02 11:26:47 +0800

    cors configuration

--- api/index.ts
+++ api/index.ts
@@ -12,10 +12,9 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
   })
 );
 
